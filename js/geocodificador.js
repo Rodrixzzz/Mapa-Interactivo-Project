61bfabc41c2e0af7bad53ac0db91a2cabd2caa44
Rodrixzzz <rodcejas@gmail.com>
2018-08-20 13:45:42 -0300

    guia 2 en progreso

--- js/geocodificador.js
+++ js/geocodificador.js
@@ -2,12 +2,22 @@ geocodificadorModulo = (function () {
   var geocodificador // Geocodificador que dada una dirección devuelve una coordenada
   
     // Permite obtener las coordenadas y las usa con la función llamada por parámtero
-  function usaDireccion (direccion, funcionALlamar) {
+  function usaDireccion (geocodificador,direccion, funcionALlamar) {
         /* Completar la función usaDireccion(dirección,funcionALlamar)
      para que se obtengan las coordenadas a partir de la dirección pasada por parámetro
      y que llame a la función pasada por parámetro con los siguientes parámetros
      dirección: la dirección pasada por parámetro
      coordenada: la ubicación de tipo google.maps.LatLng */
+
+     geocodificador.geocode({address:direccion},function(result,status){
+        if(status === 'OK'){
+          console.log(result[0].geometry.location);
+          funcionALlamar(direccion,result[0].geometry.location);
+        }
+        else{
+          console.log(status);
+        }
+     });
   }
 
     // Inicializo el geocoder que obtiene las corrdenadas a partir de una dirección
@@ -23,7 +33,7 @@ geocodificadorModulo = (function () {
       if (key === 13) { // 13 is enter
                 // code for enter
         var direccion = document.getElementById('direccion').value
-        that.usaDireccion(direccion, direccionesModulo.agregarDireccionYMostrarEnMapa)
+        that.usaDireccion(geocodificador,direccion, direccionesModulo.agregarDireccionYMostrarEnMapa)
       }
     })
   }
